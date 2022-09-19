051826722d57a0484c886356853b3581d691d47e
higuchi <1015005318@qq.com>
2022-09-19 20:52:05 +0800

    test

---
+++ src/main/java/edu/njust/dormitory/DormitoryManagementApplication.java
@@ -0,0 +1,13 @@
+package edu.njust.dormitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormitoryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/njust/dormitory/DormitoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.njust.dormitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
